3326e936fafcff9a5cc00b0b6ebf4b2c2682de5e
Christos <chris.paraskeva10@gmail.com>
2017-03-28 17:13:18 +0100

    test failing for when there are no notes

--- noteListView.js
+++ noteListView.js
@@ -3,15 +3,21 @@
 (function(exports) {
   function NoteListView(noteList) {
     this.noteList = noteList
-    console.log(this.noteList.notes())
+    // console.log(this.noteList.notes())
 
   };
 
   NoteListView.prototype.displayNotes = function () {
-    var x = this.noteList.notes().map(function(note){
-      return note.text
-    });
-    return x.join('\n');
+    console.log(this.noteList)
+    if (this.noteList.length !== 0) {
+      var notes = this.noteList.notes().map(function(note){
+        return note.text
+      });
+      return ("<ul><li><div>" + notes.join("</div></li><li><div>") + "</div></li></ul>");
+    }
+    else {
+      return 'hi'
+    }
   };
 
   exports.NoteListView = NoteListView;
--- spec/noteListViewSpec.js
+++ spec/noteListViewSpec.js
@@ -5,9 +5,25 @@ function noteListDisplayNotes() {
   var noteList = new NoteList();
   noteList.createNote("hello");
   noteList.createNote("just testing");
-
   noteListView = new NoteListView(noteList);
-  assert.isTrue(noteListView.displayNotes() === "hello" + "\n" + "just testing", "DisplayNotes method does not display current stored notes");
+  assert.isTrue(noteListView.displayNotes() === "<ul><li><div>" + "hello" + "</div></li><li><div>" + "just testing" + "</div></li></ul>", "DisplayNotes method does not display current stored notes");
 };
 
 noteListDisplayNotes();
+
+function noteListDisplayOneNote() {
+  var noteList = new NoteList();
+  noteList.createNote("hello");
+  noteListView = new NoteListView(noteList);
+  assert.isTrue(noteListView.displayNotes() === "<ul><li><div>" + "hello" + "</div></li></ul>", "DisplayNotes method does not display a single note");
+};
+
+noteListDisplayOneNote();
+
+function noteListDisplayNoNotes() {
+  var noteList = new NoteList();
+  noteListView = new NoteListView(noteList);
+  assert.isTrue(noteListView.displayNotes() === "hi", "DisplayNotes method break when no notes are saved");
+};
+
+noteListDisplayNoNotes();
