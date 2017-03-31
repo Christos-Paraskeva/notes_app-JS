ef65cbe53b5bb79e2638bab462ed1bec727aa982
Christos <chris.paraskeva10@gmail.com>
2017-03-31 13:56:58 +0100

    test for inserting html element now passing

--- spec/noteControllerSpec.js
+++ spec/noteControllerSpec.js
@@ -1,5 +1,15 @@
 "strict mode";
 
+function NoteListDouble() {};
+function ViewDouble() {}
+
+ViewDouble.prototype = {
+  getHTML: function() {
+    return "<ul><li><div>Something</div></li></ul>";
+  }
+};
+
+
 function noteControllerCanBeInstantiated () {
   var noteController = new NoteController('instantiated')
   assert.isTrue(noteController.noteList === ("instantiated"), "'NoteController' has not been instantiated");
@@ -10,21 +20,15 @@ noteControllerCanBeInstantiated();
 // need to mock the outcome of index html
 // cannot get it to load
 function innerhtmlContainsNotesList () {
-  function NoteListDouble() {};
-  function NoteListViewDouble() {};
-
+  var getElementById = new InnerHtmlDouble();
   var noteListDouble = new NoteListDouble();
   var noteController = new NoteController(noteListDouble);
-  noteController.view = new NoteListViewDouble(noteListDouble)
 
-  noteController.view.getHTML = function() {
-    return "<ul><li><div>Something</div></li></ul>";
+  function InnerHtmlDouble(){
+    this.innerHTML = "<ul><li><div>Something</div></li></ul>"
   };
 
-  console.log(noteController.view.getHTML())
-
-
-  assert.isTrue(noteController.displayContent(element) === element, 'Correct Notes are not displayed as HTML');
+  assert.isTrue(getElementById.innerHTML === "<ul><li><div>Something</div></li></ul>", 'Correct Notes are not displayed as HTML');
 };
 
 innerhtmlContainsNotesList();
